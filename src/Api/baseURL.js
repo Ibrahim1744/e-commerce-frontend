b728da68611d1d9cdc9f5de130443c25a71c7688
Ibrahim1744 <himanaser86@gmail.com>
2023-09-21 01:29:26 +0300

    change baseUrl

--- src/Api/baseURL.js
+++ src/Api/baseURL.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 
-const baseUrl = axios.create({ baseURL: "http://127.0.0.1:8000" })
+const baseUrl = axios.create({ baseURL: "https://e-commerce-just4test.onrender.com" })
 
 export default baseUrl
